bac7d4dbc647d547376c0a96e482a3e408288d1d
chithiraiselvan <d.chithiraiselvan@gmail.com>
2021-09-18 22:36:20 -0700

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import React from 'react';
-import './style.css';
+import './style.scss';
 
 export default function App() {
   return (
     <div>
-      <h1>test</h1>
-      <div>
-        <audio controls autoplay>
-          <source
-            src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
-            type="audio/mp3"
-          />
-        </audio>
-        <h1 class="absolute top-1 left-3 text-md capitalize font-light mt-1 opacity-[.85]">
-          rain
-        </h1>
-        hi
-        <audio controls autoplay>
-          <source
-            src="mixkit-little-birds-singing-in-the-trees-17.wav"
-            type="audio/wav"
-          />
-          <source
-            src="mixkit-little-birds-singing-in-the-trees-17.wav"
-            type="audio/wav"
-          />
-          Your browser does not support the audio element.
-        </audio>
+      <div class="row">
+        <div class="hex">
+          <audio  autoplay>
+            <source
+              src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+              type="audio/mp3"
+            />
+          </audio>
+        </div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+      </div>
+      <div class="row">
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+      </div>
+      <div class="row">
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+      </div>
+      <div class="row">
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
       </div>
     </div>
   );
