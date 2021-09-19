b23414db0db900ad33f1e48f72d40f521a5f7a54
chithiraiselvan <d.chithiraiselvan@gmail.com>
2021-09-19 13:43:34 -0700

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import './style.scss';
+import { jsonData } from './config';
+
+
+const tile = () => {
+  return (
+      <>
+      <div className="stock-container">
+        hi
+      {jsonData.map((data, key) => {
+      return (
+            <div key={key}>
+             {data.name}
+           </div>);
+      })}
+      </div> 
+    </>
+   );
+};
 
 export default function App() {
   return (
     <div>
-      <div class="row">
-        <div class="">
-          <audio loop controls>
-            <source
-              alt="play"
-              src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
-              type="audio/mp3"
-            />
-          </audio>
-        </div>
-        <div class="hex"></div>
-        <div class="hex"></div>
-        <div class="hex"></div>
-        <div class="hex"></div>
-      </div>
-      <div class="row">
-        <div class="hex"></div>
-        <div class="hex"></div>
-        <div class="hex"></div>
-        <div class="hex"></div>
-        <div class="hex"></div>
-      </div>
+      hi
+      <tile></tile>
+      {jsonData.map(s => (<li>hi</li>))}
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const configData = [
+    {
+      "name": "elephant",
+      "url": "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",
+      "imageurl": "'http://arbordayblog.org/wp-content/uploads/2016/06/tree.jpg"
+  },
+  {
+    "name": "lion",
+    "url": "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",
+    "imageurl": "'http://arbordayblog.org/wp-content/uploads/2016/06/tree.jpg"
+  }
+  ];
